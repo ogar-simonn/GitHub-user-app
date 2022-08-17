7b90ab95819e2711da524f6c7f254ae370c2568f
ogar-simonn <ogarsimon0000@gmail.com>
2022-08-17 23:17:03 +0100

    2 files modified

--- src/context/context.js
+++ src/context/context.js
@@ -1,14 +1,19 @@
 import React, {useState, useContext} from "react";
 import axios from "axios";
 import mockFollowers from "./mockData/mockFollowers";
-import mockRepo from "./mockData/mockRepo";
+import mockRepos from "./mockData/mockRepo";
 import mockUser from "./mockData/mockUser"
 
 const rootUrl = "https://api.github.com";
 
 const GitHubContext = React.createContext();
 const GitHubProvider = ({children}) => {
-  return <GitHubContext.Provider value="hello">{children}</GitHubContext.Provider>
+  const [githubFollowers, setGithubFollowers] = useState(mockFollowers);
+  const [repo, setRepo] = useState(mockRepos);
+  const [githubUser, setGithubUser] = useState(mockUser);
+
+
+  return <GitHubContext.Provider value={{githubFollowers, repo, githubUser}}>{children}</GitHubContext.Provider>
 }
 
 export {GitHubProvider, GitHubContext }
