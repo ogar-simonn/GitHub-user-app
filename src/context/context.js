2c771a2eb4295a8b638351073c9b762904525511
ogar-simonn <ogarsimon0000@gmail.com>
2022-08-18 23:01:35 +0100

    3 files modified

--- src/context/context.js
+++ src/context/context.js
@@ -5,14 +5,27 @@ import mockRepos from "./mockData/mockRepo";
 import mockUser from "./mockData/mockUser"
 
 const rootUrl = "https://api.github.com";
+const limitUrl = "https://api.github.com/rate_limit"
 
 const GitHubContext = React.createContext();
 const GitHubProvider = ({children}) => {
+  
   const [githubFollowers, setGithubFollowers] = useState(mockFollowers);
   const [repos, setRepos] = useState(mockRepos);
   const [githubUser, setGithubUser] = useState(mockUser);
 
-
+  const checkLimit = async () => {
+    try {
+    const response = axios(limitUrl)
+    const data = response.data;
+    console.log(data);
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  useEffect(()=> {
+    checkLimit()
+  }, [])
   return <GitHubContext.Provider value={{githubFollowers, repos, githubUser}}>{children}</GitHubContext.Provider>
 }
 
